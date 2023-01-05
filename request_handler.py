fc6a458ccb992d2efd2c78d20c4792da6b9bad74
Lauren Hanson <lohanson33@gmail.com>
2023-01-05 10:09:29 -0600

    tuple add to handlerequests

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals
+from views import get_all_animals, get_single_animal
 
 
 # Here's a class. It inherits from another class.
@@ -9,6 +9,27 @@ from views import get_all_animals
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
+
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
     # This is a Docstring it should be at the beginning of all classes and functions
     # It gives a description of the class or function
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
@@ -19,19 +40,35 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        """Handles GET requests to the server
-        """
-        # Set the response code to 'Ok'
         self._set_headers(200)
+        response = {}  # Default response
 
-        # Your new console.log() that outputs to the terminal
-        print(self.path)
+        # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
 
-        if self.path == "/animals":
-            response = get_all_animals()
-        else:
-            response = []
-            
+        if resource == "animals":
+            if id is not None:
+                response = get_single_animal(id)
+
+            else:
+                response = get_all_animals()
+
+        self.wfile.write(json.dumps(response).encode())
+
+    # def do_GET(self):
+    #     """Handles GET requests to the server
+    #     """
+    #     # Set the response code to 'Ok'
+    #     self._set_headers(200)
+
+    #     # Your new console.log() that outputs to the terminal
+    #     print(self.path)
+
+    #     if self.path == "/animals/1":
+    #         response = get_all_animals()
+    #     else:
+    #         response = []
+
         # # It's an if..else statement
         # if self.path == "/animals":
         #     # In Python, this is a list of dictionaries
