d05619268330bbefe8fba569b228ac2874d6e5ab
Lauren Hanson <lohanson33@gmail.com>
2023-01-13 13:23:14 -0600

    GET customer by email

--- request_handler.py
+++ request_handler.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse, parse_qs
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import *
@@ -10,6 +11,24 @@ from views import *
 
 class HandleRequests(BaseHTTPRequestHandler):
     # Here's a class function
+    # replace the parse_url function in the class
+    def parse_url(self, path):
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split('/')  # ['', 'animals', 1]
+        resource = path_params[1]
+
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
+        try:
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)
+    """
     def parse_url(self, path):
         # Just like splitting a string in JavaScript. If the
         # path is "/animals/1", the resulting list will
@@ -31,7 +50,41 @@ class HandleRequests(BaseHTTPRequestHandler):
         return (resource, id)  # This is a tuple
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
+"""
 
+    def do_GET(self):
+        self._set_headers(200)
+
+        response = {}
+
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
+
+        # If the path does not include a query parameter, continue with the original if block
+        if '?' not in self.path:
+            (resource, id) = parsed
+
+            """if resource == "animals":
+                if id is not None:
+                    response = f"{get_single_animal(id)}"
+                else:
+                    response = f"{get_all_animals()}"""
+            if resource == "customers":
+                if id is not None:
+                    response = f"{get_single_customer(id)}"
+                else:
+                    response = f"{get_all_customers()}"
+
+        else:  # There is a ? in the path, run the query param functions
+            (resource, query) = parsed
+
+            # see if the query dictionary has an email key
+            if query.get('email') and resource == 'customers':
+                response = get_customer_by_email(query['email'][0])
+
+        self.wfile.write(json.dumps(response).encode())
+
+    """
     def do_GET(self):
         response = {}  # Default response
 
@@ -48,7 +101,7 @@ class HandleRequests(BaseHTTPRequestHandler):
                     response = "This animal is not home"
                 else:
                     self._set_headers(200)
-                    
+
             else:
                 self._set_headers(200)
                 response = get_all_animals()
@@ -83,7 +136,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         #         response = "HI"
 
         self.wfile.write(json.dumps(response).encode())
-
+"""
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     # def do_POST(self):
